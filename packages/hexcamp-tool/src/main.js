2ae9735a5ce4e5d66a51c383163e709c42e71a90
Jim Pick <jim@jimpick.com>
2025-07-30 14:21:45 -0700

    Use cmd-ts to process command line

--- packages/hexcamp-tool/src/main.js
+++ packages/hexcamp-tool/src/main.js
@@ -1,10 +1,23 @@
 #!/usr/bin/env node
 
+import { command, subcommands, run, string, positional } from 'cmd-ts';
 import { ehidToH3Hex } from '@hexcamp/hex-utils';
 
-const ehid = process.argv[2];
-if (!ehid) {
-  console.error('Need ehid')
-  process.exit(1);
-}
-console.log(ehidToH3Hex(ehid));
+const ehidToH3HexCmd = command({
+  name: 'ehid-to-h3-hex',
+  args: {
+    ehid: positional({ type: string, displayName: 'Encoded Hexagon ID' }),
+  },
+  handler: ({ ehid }) => {
+    console.log(ehidToH3Hex(ehid));
+  },
+});
+
+const hexcampTool = subcommands({
+  name: 'hexcamp-tool',
+  cmds: { 
+    'ehid-to-h3-hex': ehidToH3HexCmd
+  },
+});
+
+run(hexcampTool, process.argv.slice(2));
